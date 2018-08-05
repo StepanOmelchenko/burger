a7d2fcbf97f42b16c184d2a20cfb1ceec891b81b
Stepan <omelchenko.stepan@gmail.com>
2018-08-05 10:57:55 +0600

    #3 map section mobile bg added

--- src/lib/map.js
+++ src/lib/map.js
@@ -1,42 +1,48 @@
 // map
+const mapSection = document.querySelector('#map');
+const mapWidth = mapSection.getBoundingClientRect().width;
 
-ymaps.ready(init);
-var myMap;
-var coords = [
-  [59.961368, 30.288778],
-  [59.913992, 30.302168],
-  [59.944665, 30.373579],
-  [59.931572, 30.435377]
-];
+if (mapWidth >= 480) {  
+  ymaps.ready(init);
+  var myMap;
+  var coords = [
+    [59.961368, 30.288778],
+    [59.913992, 30.302168],
+    [59.944665, 30.373579],
+    [59.931572, 30.435377]
+  ];
 
-function init(){     
-  myMap = new ymaps.Map("map", {
-    center: [59.941392, 30.293756],
-    zoom: 12,
-    controls: ['zoomControl']
-  });  
-  myMap.behaviors.disable('scrollZoom');
+  function init(){     
+    myMap = new ymaps.Map("map", {
+      center: [59.941392, 30.293756],
+      zoom: 12,
+      controls: ['zoomControl']
+    });  
+    myMap.behaviors.disable('scrollZoom');
 
-  var placemarks = createPlacemarks(coords);
+    var placemarks = createPlacemarks(coords);
 
-  placemarks.forEach((placemark) => {
-    myMap.geoObjects.add(placemark);
-  });
-
-  function createPlacemarks(array) {
-    let placemarks = [];
-  
-    array.forEach((coord) => {
-      let oneMark = new ymaps.Placemark([coord[0], coord[1]], {}, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/map-marker.png',
-        iconImageSize: [46, 57],
-        iconImageOffset: [0, 0]
-      });  
-      placemarks.push(oneMark);
+    placemarks.forEach((placemark) => {
+      myMap.geoObjects.add(placemark);
     });
-  
-    return placemarks;
+
+    function createPlacemarks(array) {
+      let placemarks = [];
+    
+      array.forEach((coord) => {
+        let oneMark = new ymaps.Placemark([coord[0], coord[1]], {}, {
+          iconLayout: 'default#image',
+          iconImageHref: 'img/icons/map-marker.png',
+          iconImageSize: [46, 57],
+          iconImageOffset: [0, 0]
+        });  
+        placemarks.push(oneMark);
+      });
+    
+      return placemarks;
+    }
+    
   }
-  
+} else {
+  mapSection.classList.add('eighth__map--mobile');
 }
